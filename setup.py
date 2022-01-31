2fad03d7ef87d984dfaa771c44a1e903e4325ddc
pawel <pawel@devsti.me>
2022-01-31 10:56:46 +0100

    feature: setup.py integration

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hikvision-configure",
+    version="0.0.1",
+    url="https://github.com/synweap15/hikvision-configure",
+    author="qb60",
+    description="Hikvision/HiWatch camera configuring script",
+    packages=find_packages(),
+    install_requires=[
+        "certifi==2020.11.8",
+        "chardet==3.0.4",
+        "idna==2.10",
+        "pycryptodomex==3.9.9",
+        "requests==2.25.0",
+        "urllib3==1.26.2",
+    ],
+)
